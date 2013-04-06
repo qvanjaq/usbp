f6ddbfbf111921499a70a31a062152aa4ab7eb04
Иван <qvanjaq@qvanjaq-GA-MA69GM-S2H.(none)>
2013-04-06 13:56:25 +0300

    carcase modules and design application

---
+++ protected/modules/main/modules/front/controllers/PagesController.php
@@ -0,0 +1,9 @@
+<?php
+
+class PagesController extends Controller
+{
+	public function actionTextFiles()
+	{    
+		$this->render('index');
+	}
+}
---
+++ protected/modules/main/modules/front/views/layouts/main.php
@@ -0,0 +1,20 @@
+<?php $this->beginContent('application.modules.main.views.layouts.main');; ?>
+<div id="mainContainer">
+    <div id="mainContainer2">
+        <div id="header">
+            <div id="mainMenu">
+                <ul>
+                    <li><a>Images</a></li>
+                    <li><a>Texts</a></li>    
+                </ul>
+            </div>
+        </div>
+        <div id="main">
+        <?php echo $content; ?>
+        </div>
+    </div>
+</div>
+<div id="footer">
+    Copyright © <?php echo date('Y') ?>.  all rights reserved.
+</div>
+<?php $this->endContent(); ?>
